144721b0af215882b6dd45cbe9fa1dfa3da2f97e
EbMian <Ebrotie_Mian@etu.u-bourgogne.fr>
2024-01-10 11:33:17 +0100

    O as Open/Closed Principle

---
+++ exo-solid-2/Mp3.php
@@ -0,0 +1,11 @@
+<?php
+/*use ReaderType;*/
+require_once 'MusicType.php';
+class Mp3 extends MusicType
+{
+
+    public function listen()
+    {
+        return 'Lecture du fichier Mp3 '. $this->filename;
+    }
+}
---
+++ exo-solid-2/MusicReader.php
@@ -0,0 +1,33 @@
+<?php
+
+// Si on doit supporter un nouveau type de format, on doit modifier cette classe :(
+require_once 'Mp3.php';
+require_once 'Ogg.php';
+
+class MusicReader
+{
+    private $filename;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    
+    public function listen()
+    {
+        $extension = pathinfo($this->filename, PATHINFO_EXTENSION);
+        switch ($extension) {
+            case 'mp3':
+                $mp3 = new Mp3($this->filename);
+                return $mp3->listen();
+               
+            case 'ogg':
+                $ogg = new Ogg($this->filename);
+                return $ogg->listen();
+                
+            default:
+                throw new \Exception('Aucun lecteur trouvé pour cette musique');
+        }
+    }
+}
